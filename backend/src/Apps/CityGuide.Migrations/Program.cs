8ecab980312b67008a5feb7a859f314b518000e3
Bartosz Selwesiuk <bselwesiuk@gmail.com>
2018-11-06 19:39:54 +0100

    Initialize backend project

---
+++ backend/src/Apps/CityGuide.Migrations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CityGuide.Migrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
